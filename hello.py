bd678c1597b13544797b744fa1d0c2f3ce91e495
Le Van Phuc <levanphuc18112001@gmail.com>
2024-06-17 18:31:29 +0700

    Success32

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
-def  hello():
-     print("132")
-def  test():
-     print("dhasjdh")
-     print("2iuo12i")
-     print("2iuo12i")
-     print("2iuo312313")
+def hello():
+    print("132")
+
+
+def test():
+    print("dhasjdh")
+    print("2iuo12i")
+    print("2iuo12i")
+    print("2iuo312313")
+
